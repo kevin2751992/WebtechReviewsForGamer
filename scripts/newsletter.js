49bd98e4c60cc9072ef18532aaac2ad98c59f2a7
Username <email@adress.com>
2019-11-22 10:07:36 +0100

    newsletter

---
+++ scripts/newsletter.js
@@ -0,0 +1,9 @@
+var name = document.getElementById("name").value;
+var email = document.getElementById("mail").value;
+
+function testName(name) {
+    for(var i = 0; i < name.length; i++) 
+    {
+        
+    }
+  }
