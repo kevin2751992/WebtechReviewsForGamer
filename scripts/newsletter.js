081c6372152b1bf045099de3b1613ef63b0c7593
Username <email@address.com>
2019-11-26 14:53:22 +0100

    newsletter seite ohne responsive design

--- scripts/newsletter.js
+++ scripts/newsletter.js
@@ -1,9 +1,23 @@
-var name = document.getElementById("name").value;
-var email = document.getElementById("mail").value;
+var regexName = /^[A-Za-z0-9äöüÄÖÜß \-'.]+$/;
+var regexMail = /\b[A-Za-z0-9!#$%&'*+-/=?^_`{|}~]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/;
 
-function testName(name) {
-    for(var i = 0; i < name.length; i++) 
-    {
-        
+function testInput() {
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('mail').value;
+  if(regexName.test(name)) {
+    if(regexMail.test(email)) {
+      alert("Newsletter erfolgreich abonniert!");
+    }
+    else {
+      alert("Bitte überprüfe deine E-Mail nochmal!");
     }
   }
+  else {
+    if(regexMail.test(email)) {
+      alert("Bitte überprüfe deinen Namen nochmal!");
+    }
+    else {
+      alert("Bitte überprüfe deinen Namen und deine E-Mail nochmal!");
+    }
+  }
+}
