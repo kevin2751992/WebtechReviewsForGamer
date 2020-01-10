aa2b2722d49a73d045877a1119b5463fb60603f1
DerEiermann <konrad.draeger.1@gmx.de>
2020-01-10 19:41:45 +0100

    new branch

---
+++ scripts/sound.js
@@ -0,0 +1,24 @@
+function PlayOkay()
+{
+document.getElementById('audiofile').play();
+}
+
+function PlayFirstname()
+{
+document.getElementById('audiofile2').play();
+}
+
+function PlayLastname()
+{
+document.getElementById('audiofile3').play();
+}
+
+function PlayEmail()
+{
+document.getElementById('audiofile4').play();
+}
+
+function PlayMessage()
+{
+document.getElementById('audiofile5').play();
+}
